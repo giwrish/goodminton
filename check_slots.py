25efeb1454ef066a4d7e5de25e1a6e5e920acc08
shindegirish <shinde.girish.r@gmail.com>
2024-10-21 23:17:33 +0200

    removing logger

--- check_slots.py
+++ check_slots.py
@@ -1,13 +1,10 @@
 import requests
 import os
-import logging
 from requests.exceptions import RequestException
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
-logging.basicConfig(level=logging.INFO, filename='availability_checker.log', filemode='a',
-                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 bot_token = os.getenv('TELEGRAM_BOT_TOKEN') 
 chat_id = os.getenv('TELEGRAM_CHAT_ID')
@@ -30,11 +27,11 @@ def send_alert():
     try:
         response = requests.get(url)
         if response.status_code == 200:
-            logging.info("Telegram alert sent successfully")
+            print("Telegram alert sent successfully")
         else:
-            logging.error(f"Failed to send alert. Status code: {response.status_code}")
+            print(f"Failed to send alert. Status code: {response.status_code}")
     except RequestException as e:
-        logging.error(f"Error sending Telegram alert: {e}")
+       print(f"Error sending Telegram alert: {e}")
 
 def check_availability(start_date):
     url = f'https://www.eversports.de/api/slot?facilityId=76443&startDate={start_date}&courts[]=77394&courts[]=77395&courts[]=77396'
@@ -47,20 +44,17 @@ def check_availability(start_date):
         
         count = len(occurrences)
         
-        logging.info(f"Occurrences found: {count}")
+        print(f"Courts available: {3-count}")
         
         if count < MIN_OCCURRENCES:
-            logging.warning("Less than 3 occurrences found, court is available")
             send_alert()
-        else:
-            logging.info(f"all courts are booked")
     
     except RequestException as e:
-        logging.error(f"Failed to fetch data: {e}")
+        print(f"Failed to check slots: {e}")
 
 def main():
     start_date = get_last_thursday_within_next_14_days()
-    logging.info(f"Starting availability checker for date: {start_date}")
+    print(f"Starting availability checker for date: {start_date}")
     check_availability(start_date)
 
 if __name__ == "__main__":
